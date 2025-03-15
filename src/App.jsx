f2bf9f7dc17690280a6cf1f9a8cffeb330de7fc5
thijsdenbutter <thijsdenbutter@gmail.com>
2025-03-15 11:55:22 +0100

    button component gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import Home from './Pages/home/Home.jsx'
 import NewPost from './Pages/new-post/NewPost.jsx'
 import Posts from './Pages/posts/Posts.jsx'
@@ -20,7 +20,8 @@ function App() {
                     <Route path="*" element={<PageNotFound/>}/>
                 </Routes>
             </div>
-    </>
+        </>
     )
 }
+
 export default App
--- src/components/post-tile/PostTile.jsx
+++ src/components/post-tile/PostTile.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function PostTile({id, title, author, comments, shares }) {
     return (
-        <Link className="post-tile" to={`./${id}`}>
+        <Link className="post-tile" to={`./${id - 1}`}>
             <h2>{title} (<cite>{author}</cite>)</h2>
             <p>{comments} reacties - {shares} gedeeld</p>
         </Link>
